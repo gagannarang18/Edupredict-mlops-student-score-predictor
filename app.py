cdc7985679096ee67ad5e141346ea698fd3afa2a
Gagan Narang <144628265+gagannarang18@users.noreply.github.com>
2025-04-21 21:14:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from sec.pipeline.predict_pipeline import CustomData,PredictPipeline
 
